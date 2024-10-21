2d2d03cfecb3258645bb2dae01852a983ac6c0db
Naveen <126401411+naveenghub@users.noreply.github.com>
2024-10-21 18:34:55 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser hence this alert will not work
+
+console.log(3 + 3); 
+console.log("Naveen")
+
+let name = "Naveen"
+let age = 20
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true / false
+// null => standalone value which is also object type
+// undefined => value is not yet defined
+// symbol => when defining unique values
+
+// object
+
+console.log(typeof "Naveen"); // this will print type of data of Naveen, which is string.
+console.log(typeof null); // this will print data type of null, which is object.
